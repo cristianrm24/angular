91970f25d6463d0da635faa5e299f42b59fc9217
Cristian Damian <soliscris72@gmail.com>
2026-01-05 13:17:43 -0600

    modificion de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ProductoListComponent } from 'C:\Users\Cristian Damian\Desktop\proyecto_back\proyecto_back\src\app\features\productos\producto-list\producto-list\producto-list.component.ts';
+import { ProductoListComponent } from './features/productos/producto-list/producto-list/producto-list.component';
 
 export const routes: Routes = [
   {
